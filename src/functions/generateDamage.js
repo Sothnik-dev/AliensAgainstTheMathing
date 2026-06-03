bccb8920d15587717fa11fcff12242c246f7377b
Sothnik <sothnikdev@proton.me>
2026-06-03 16:40:03 -0300

    code optimization and some visual improvements

--- src/functions/generateDamage.js
+++ src/functions/generateDamage.js
@@ -5,11 +5,11 @@ import { Weapon } from "../components/weapon.js";
 import { NewNumber } from "./newNumber.js";
 import { randomMath } from "./randomMath.js";
 
+import { deathDefinition } from "./winDefeatCondition.js";
+import { victoryCase } from "./winDefeatCondition.js";
+
 //Define de quem é a vez, true = p1 e false = p2
 export let playerRound = true;
-
-//Define quem venceu, segue a mesma lógica
-let playerVictorious = undefined;
 
 //Recebimento de variáveis externas
 const player1NameValue = sessionStorage.getItem('player1name');
@@ -30,15 +30,9 @@ const player2Skin = document.getElementsByClassName('player')[1];
 
 const confirmBtn = document.getElementById('confirmBtn');
 const startBtn = document.getElementById('startBtn');
-
 const answare = document.getElementById('answare');
 
 const timer = document.getElementById('nowTime');
-
-const ending = document.getElementById('endingContainer');
-const winMessage = document.getElementById('winMessage');
-const victoryText = document.getElementById('victoryText');
-winMessage.style.visibility = 'hidden';
  
 //Algum dia eu otimizo isso
 let fValue = Math.floor(Math.random() * 10) + 1;
@@ -69,88 +63,6 @@ export function selfInflictDamage() {
     } else {
         player2.damageOutput(gunWeapon.damage, gunWeapon.kritzProb);
         player2Health.value = player2.health;
-    }
-}
-
-//Define o estado de morte através do life do player
-export function deathDefinition() {
-    if (player1.health <= 0) {
-        playerVictorious = false;
-        
-        player1Skin.style.filter = `drop-shadow(0px 0px 0px green)`
-        player2Skin.style.filter = `drop-shadow(0px 0px 0px green)`
-
-        switch (player1NameValue) {
-            case 'Blormall':
-                player1Skin.src = `/public/imgs/aliensSkins/aliensnshideathv1.png`;
-                break;
-            case 'Cröwboy':
-                player1Skin.src = `/public/imgs/aliensSkins/aliensnshideathv2.png`;
-                break;
-            case 'Bread Burst':
-                player1Skin.src = `/public/imgs/aliensSkins/aliensnshideathv3.png`;
-                break;
-        }
-    } else if (player2.health <= 0) {
-        playerVictorious = true;
-
-        player1Skin.style.filter = `drop-shadow(0px 0px 0px green)`
-        player2Skin.style.filter = `drop-shadow(0px 0px 0px green)`
-        
-        switch (player2NameValue) {
-            case 'Blormall':
-                player2Skin.src = `/public/imgs/aliensSkins/aliensnshideathv1.png`;
-                break;
-            case 'Cröwboy':
-                player2Skin.src = `/public/imgs/aliensSkins/aliensnshideathv2.png`;
-                break;
-            case 'Bread Burst':
-                player2Skin.src = `/public/imgs/aliensSkins/aliensnshideathv3.png`;
-                break;
-        }
-    }
-}
-
-//Define o bool do vitorioso através do derrotado
-export function victoryCase() {
-    if (playerVictorious == true) {
-        switch(player1NameValue) {
-            case 'Blormall':
-                player1Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv1.png`;
-                break;
-            case 'Cröwboy':
-                player1Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv2.png`;
-                break;
-            case 'Bread Burst':
-                player1Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv3.png`;
-                break;
-        }
-        endingContainer.style.visibility = `hidden`;
-        timer.style.visibility = `hidden`;
-        winMessage.style.visibility = `visible`;
-        victoryText.src = `/public/imgs/deco/winStatep1.png`
-        victoryText.addEventListener('click', () => {
-            window.location.href = `/public/CharacterSelection/chrSelection.html`;
-        })
-    } else if (playerVictorious == false) {
-        switch(player2NameValue) {
-            case 'Blormall':
-                player2Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv1.png`;
-                break;
-            case 'Cröwboy':
-                player2Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv2.png`;
-                break;
-            case 'Bread Burst':
-                player2Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv3.png`;
-                break;
-        }    
-        endingContainer.style.visibility = `hidden`;
-        timer.style.visibility = `hidden`;
-        winMessage.style.visibility = `visible`;
-        victoryText.src = `/public/imgs/deco/winStatep2.png`  
-        victoryText.addEventListener('click', () => {
-            window.location.href = `/public/CharacterSelection/chrSelection.html`;
-        })
     }
 }
 
---
+++ src/functions/winDefeatCondition.js
@@ -0,0 +1,103 @@
+//Define quem venceu, segue a mesma lógica
+let playerVictorious = undefined;
+
+//Captura de valores através de DOM
+const player1Skin = document.getElementsByClassName('player')[0];
+const player2Skin = document.getElementsByClassName('player')[1];
+
+const ending = document.getElementById('endingContainer');
+const winMessage = document.getElementById('winMessage');
+const victoryText = document.getElementById('victoryText');
+winMessage.style.visibility = 'hidden';
+
+const timerContainer = document.getElementById('nowTimeStyle');
+
+//Captura da barra de vida dos jogadores (progress)
+const player1Health = document.getElementById('player1Health');
+const player2Health = document.getElementById("player2Health");
+
+//Recebimento de variáveis externas
+const player1NameValue = sessionStorage.getItem('player1name');
+const player2NameValue = sessionStorage.getItem('player2name');
+
+//Define o estado de morte através do life do player
+export function deathDefinition() {
+    if (player1Health.value <= 0) {
+        playerVictorious = false;
+        
+        player1Skin.style.filter = `drop-shadow(0px 0px 0px green)`
+        player2Skin.style.filter = `drop-shadow(0px 0px 0px green)`
+
+        switch (player1NameValue) {
+            case 'Blormall':
+                player1Skin.src = `/public/imgs/aliensSkins/aliensnshideathv1.png`;
+                break;
+            case 'Cröwboy':
+                player1Skin.src = `/public/imgs/aliensSkins/aliensnshideathv2.png`;
+                break;
+            case 'Bread Burst':
+                player1Skin.src = `/public/imgs/aliensSkins/aliensnshideathv3.png`;
+                break;
+        }
+    } else if (player2Health.value <= 0) {
+        playerVictorious = true;
+
+        player1Skin.style.filter = `drop-shadow(0px 0px 0px green)`
+        player2Skin.style.filter = `drop-shadow(0px 0px 0px green)`
+        
+        switch (player2NameValue) {
+            case 'Blormall':
+                player2Skin.src = `/public/imgs/aliensSkins/aliensnshideathv1.png`;
+                break;
+            case 'Cröwboy':
+                player2Skin.src = `/public/imgs/aliensSkins/aliensnshideathv2.png`;
+                break;
+            case 'Bread Burst':
+                player2Skin.src = `/public/imgs/aliensSkins/aliensnshideathv3.png`;
+                break;
+        }
+    }
+}
+
+//Define o bool do vitorioso através do derrotado
+export function victoryCase() {
+    if (playerVictorious == true) {
+        switch(player1NameValue) {
+            case 'Blormall':
+                player1Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv1.png`;
+                break;
+            case 'Cröwboy':
+                player1Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv2.png`;
+                break;
+            case 'Bread Burst':
+                player1Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv3.png`;
+                break;
+        }
+        endingContainer.style.visibility = `hidden`;
+        timerContainer.style.visibility = `hidden`;
+        winMessage.style.visibility = `visible`;
+        victoryText.src = `/public/imgs/deco/winStatep1.png`
+        victoryText.addEventListener('click', () => {
+            window.location.href = `/public/CharacterSelection/chrSelection.html`;
+        })
+    } else if (playerVictorious == false) {
+        switch(player2NameValue) {
+            case 'Blormall':
+                player2Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv1.png`;
+                break;
+            case 'Cröwboy':
+                player2Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv2.png`;
+                break;
+            case 'Bread Burst':
+                player2Skin.src = `/public/imgs/aliensSkins/aliensnshivictoryv3.png`;
+                break;
+        }    
+        endingContainer.style.visibility = `hidden`;
+        timerContainer.style.visibility = `hidden`;
+        winMessage.style.visibility = `visible`;
+        victoryText.src = `/public/imgs/deco/winStatep2.png`  
+        victoryText.addEventListener('click', () => {
+            window.location.href = `/public/CharacterSelection/chrSelection.html`;
+        })
+    }
+}
