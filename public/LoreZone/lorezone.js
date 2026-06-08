ffea2b857ee754b7b5c2bdacbe8db1e0999ba735
Sothnik <sothnikdev@proton.me>
2026-06-07 23:33:23 -0300

    slow progress today :/

--- public/LoreZone/lorezone.js
+++ public/LoreZone/lorezone.js
@@ -1,6 +1,7 @@
 const lb1 = document.getElementsByClassName('loreBlock')[0];
 const lb2 = document.getElementsByClassName('loreBlock')[1];
 const lb3 = document.getElementsByClassName('loreBlock')[2];
+const lb4 = document.getElementsByClassName('loreBlock')[3];
 
 let pageValue = 1;
 
@@ -9,4 +10,5 @@ switch (pageValue) {
         lb1.src = `/public/imgs/loreImgs/loreBlock1Val1.png`
         lb2.src = `/public/imgs/loreImgs/loreBlock2Val1.png`
         lb3.src = `/public/imgs/loreImgs/loreBlock3Val1.png`
+        lb4.src = `/public/imgs/loreImgs/loreBlock4Val1.png`
 }
